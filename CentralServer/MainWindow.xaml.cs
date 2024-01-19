63873a21a21de00e52bb2c9ce085f227cb605cc5
Arseniio <42149853+Arseniio@users.noreply.github.com>
2024-01-19 19:48:45 +0500

    partially modified code to work with JSON

--- CentralServer/MainWindow.xaml.cs
+++ CentralServer/MainWindow.xaml.cs
@@ -44,79 +44,69 @@ namespace CentralServer {
                         var stationId = Convert.ToInt32(request.Url.Query.Split('=')[1]);
                         Logs.Text += "\ngetStationInfo; ID = " + stationId;
                         //find correct station
-                        var station = context.Fuel_List.ToList().Where(p => p.Station_ID == stationId).ToList();
+                        var station = context.Gas_Station_ID.ToList().Where(p => p.ID == stationId).ToList();
                         if (station.Count != 0) {
                             Logs.Text += "\nStations records Found: " + station.Count;
-                            StringBuilder fueldata = new StringBuilder();
-                            fueldata.AppendLine("addr:" + context.Gas_Station_ID.ToList().Where(p => p.ID == stationId).ToList().First().Address);
-                            foreach (var item in station) {
-                                Logs.Text += "\n" + String.Format("{0} at {1}L costs {2} at sql ID {3}", item.Name, item.AmountOfFuel, item.Price, item.ID);
-                                //building data for sending to server
-                                fueldata.AppendLine(String.Format("{0}:{1}:{2}:{3}", item.Name, item.AmountOfFuel, item.Price, item.ID));
-                            }
+                            string jsonData = JsonSerializer.Serialize(station.First(),new JsonSerializerOptions(){ ReferenceHandler = ReferenceHandler.IgnoreCycles });
                             //send data to control app
-                            serverContext.Response.Close(Encoding.UTF8.GetBytes(fueldata.ToString()), false);
+                            serverContext.Response.Close(Encoding.UTF8.GetBytes(jsonData.ToString()), false);
                         }
                         else {
-                            Logs.Text += "\nNo such station found";
-                            ///TODO: creating new station not implemented yet
-                            ///need to make new station address TextBox writable if StatusCode == 500 
-                            ///on Control project
-                            serverContext.Response.StatusCode = 500;
-                            serverContext.Response.Close();
-
+                            stationId = Convert.ToInt32(request.Url.Query.Split('=')[1]);
+                            Logs.Text += "\nNo " + stationId + " station found, creating new";
+                            serverContext.Response.Close(Encoding.UTF8.GetBytes(stationId.ToString()), false);
                         }
                         break;
                     case "setStation":
                         Logs.AppendText("\nGot POST reqest");
                         string content = null;
-                        Fuel_List overrideStation = new Fuel_List();
-                        Gas_Station_ID overrideStationID = new Gas_Station_ID();
+                        string newAddr = "";
                         using (var reader = new StreamReader(request.InputStream))
                             content = reader.ReadToEnd();
+                        ///TODO:
+                        ///Rewrite this part of code to work with JSON structure
                         var resSplit = content.Split('\n');
+                        int currStationID = 0;
+                        Gas_Station_ID gas_Station_ID = new Gas_Station_ID();
                         foreach (var line in resSplit) {
                             var tokens = line.Split(':');
                             switch (tokens[0]) {
                                 case "id":
-                                    overrideStationID.ID = int.Parse(tokens[1]);
-                                    overrideStation.Station_ID = int.Parse(tokens[1]);
+                                    currStationID = Convert.ToInt32(tokens[1]);
                                     break;
                                 case "": break;
                                 case "addr":
-                                    overrideStationID.Address = tokens[1];
+                                    Gas_Station gas_Station = new Gas_Station();
+                                    gas_Station.ID = Convert.ToInt32(currStationID);
+                                    gas_Station.Gas_Station_ID = gas_Station_ID;
+                                    gas_Station.Gas_station1 = "test";
+                                    gas_Station_ID.Address = tokens[1];
+                                    gas_Station_ID.ID = currStationID;
+
+                                    GasStationdbEntities.GetContext().Gas_Station.AddOrUpdate(gas_Station);
+                                    GasStationdbEntities.GetContext().Gas_Station_ID.AddOrUpdate(gas_Station_ID);
                                     break;
                                 default:
                                     Fuel_List editedEntry = GasStationdbEntities.GetContext().Fuel_List.ToList().Find(
                                         (p) => p.ID == int.Parse(tokens[3])
                                         );
+                                    if (editedEntry == null) {
+                                        editedEntry = new Fuel_List();
+                                        editedEntry.ID = 0;
+                                    }
                                     if (editedEntry != null) {
                                         editedEntry.Name = tokens[0];
+                                        editedEntry.Gas_Station_ID = gas_Station_ID;
+                                        editedEntry.Station_ID = currStationID;
                                         editedEntry.Price = decimal.Parse(tokens[2]);
                                         editedEntry.AmountOfFuel = int.Parse(tokens[1]);
                                     }
                                     GasStationdbEntities.GetContext().Fuel_List.AddOrUpdate(editedEntry);
-                                    GasStationdbEntities.GetContext().SaveChanges();
-                                    var modifList = GasStationdbEntities.GetContext().Fuel_List.ToList();
-
-                                    //overrideStation.Name = tokens[0];
-                                    //overrideStation.Price = decimal.Parse(tokens[2]);
-                                    //overrideStation.AmountOfFuel = int.Parse(tokens[1]);
-                                    //overrideStation.ID = int.Parse(tokens[3]);
-                                    ////push changes to db
-                                    //GasStationdbEntities.GetContext().Fuel_List.Remove(
-                                    //    GasStationdbEntities.GetContext().Fuel_List.ToList().Find(
-                                    //        (p) => p.ID == overrideStation.ID
-                                    //    )
-                                    //    );
-                                    //GasStationdbEntities.GetContext().Fuel_List.Add(overrideStation);
-                                    ////GasStationdbEntities.GetContext().Fuel_List.Add(overrideStation);
                                     break;
                             }
                             //push new address to db
-                            GasStationdbEntities.GetContext().Gas_Station_ID.AddOrUpdate(overrideStationID);
-
                         }
+                        GasStationdbEntities.GetContext().SaveChanges();
                         break;
                 }
                 Logs.ScrollToEnd();
--- GasStation/GasStationControl/DataPage.xaml.cs
+++ GasStation/GasStationControl/DataPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,7 +28,9 @@ namespace GasStationControl {
     public partial class DataPage : Page {
         HttpClient httpClient = null;
         string StationID;
-        List<int> dbRecords = new List<int>();
+        bool newStation = false;
+        JsonNode jsonData = null;
+        JsonNode fuelData = null;
         public DataPage(string stationID) {
             InitializeComponent();
             StationID = stationID;
@@ -37,34 +42,31 @@ namespace GasStationControl {
                 return;
             }
             data = httpResponse.Content.ReadAsStringAsync().Result;
+            if (data == stationID) {
+                newStation = true;
+                return;
+            }
             //Response parser
-            var resSplit = data.Split('\n');
-            foreach (var line in resSplit) {
-                var tokens = line.Split(':');
-                switch (tokens[0]) {
-                    case "addr":
-                        Address.Text = tokens[1];
-                        break;
+            jsonData = JsonNode.Parse(data);
+            Address.Text = jsonData!["Address"].ToString();
+            fuelData = jsonData!["Fuel_List"];
+            foreach (var record in fuelData.AsArray()) {
+                switch (record!["Name"].ToString()) {
                     case "92":
-                        TBox92_amount.Text = tokens[1];
-                        TBox92_cost.Text   = tokens[2];
-                        dbRecords.Add(int.Parse(tokens[3]));
+                        TBox92_amount.Text = record!["AmountOfFuel"].ToString();
+                        TBox92_cost.Text = record!["Price"].ToString();
                         break;
                     case "95":
-                        TBox95_amount.Text = tokens[1];
-                        TBox95_cost.Text   = tokens[2];
-                        dbRecords.Add(int.Parse(tokens[3]));
+                        TBox95_amount.Text = record!["AmountOfFuel"].ToString();
+                        TBox95_cost.Text = record!["Price"].ToString();
                         break;
                     case "98":
-                        TBox98_amount.Text = tokens[1];
-                        TBox98_cost.Text   = tokens[2];
-                        dbRecords.Add(int.Parse(tokens[3]));
+                        TBox98_amount.Text = record!["AmountOfFuel"].ToString();
+                        TBox98_cost.Text = record!["Price"].ToString();
                         break;
                     case "Disel Fuel":
-                        TBoxDF_amount.Text = tokens[1];
-                        //
-                        TBoxDF_cost.Text   = tokens[2];
-                        dbRecords.Add(int.Parse(tokens[3]));
+                        TBoxDF_amount.Text = record!["AmountOfFuel"].ToString();
+                        TBoxDF_cost.Text = record!["Price"].ToString();
                         break;
                 }
             }
@@ -74,14 +76,27 @@ namespace GasStationControl {
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e) {
-            StringBuilder sendData = new StringBuilder();
-            sendData.AppendLine(String.Format("id:{0}", StationID));
-            sendData.AppendLine(String.Format("addr:{0}", Address.Text));
-            sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "92", TBox92_amount.Text, TBox92_cost.Text, dbRecords[0]));
-            sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "95", TBox95_amount.Text, TBox95_cost.Text, dbRecords[1]));
-            sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "98", TBox98_amount.Text, TBox98_cost.Text, dbRecords[2]));
-            sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "Disel Fuel", TBoxDF_amount.Text, TBoxDF_cost.Text, dbRecords[3]));
-            var response = httpClient.PostAsync("setStation", new StringContent(sendData.ToString()));
+            var i = 0;
+            jsonData!["Address"] = Address.Text;
+            foreach (var fuelType in new[] { "92","95","98","DF" }) {
+                ///чат гпт интересную конструкцию подсказал(не особо фанат,использую только пока json изучаю),
+                ///это подставит название из верхнего массива и вернёт TextBox
+                ///так же для примерно такой же штуки можно использовать nameof,вернёт название переменной как строку :)
+                TextBox TBAmount = FindName($"TBox{fuelType}_amount") as TextBox;
+                TextBox TBCost = FindName($"TBox{fuelType}_cost") as TextBox;
+                fuelData.AsArray()[i]!["AmountOfFuel"] = TBAmount.Text;
+                fuelData.AsArray()[i]!["Price"] = TBCost.Text;
+                i++;
+            }
+            var debug = jsonData.ToString();
+            var response = httpClient.PostAsync("setStation", new StringContent(jsonData.ToString()));
+            //StringBuilder sendData = new StringBuilder();
+            //sendData.AppendLine(String.Format("id:{0}", StationID));
+            //sendData.AppendLine(String.Format("addr:{0}", Address.Text));
+            //sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "92", TBox92_amount.Text, TBox92_cost.Text, dbRecords[0]));
+            //sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "95", TBox95_amount.Text, TBox95_cost.Text, dbRecords[1]));
+            //sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "98", TBox98_amount.Text, TBox98_cost.Text, dbRecords[2]));
+            //sendData.AppendLine(String.Format("{0}:{1}:{2}:{3}", "Disel Fuel", TBoxDF_amount.Text, TBoxDF_cost.Text, dbRecords[3]));
 
         }
     }
